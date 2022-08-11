b8b378a4ba1acd9724ca62a9d83572af948cc695
Stephan Raaijmakers <stephan.raaijmakers@tno.nl>
2022-08-11 15:59:32 +0200

    Update

--- COMMIT.sh
+++
@@ -1,5 +0,0 @@
-#git remote add origin git@github.com:stephanraaijmakers/deeplearningfornlp.git
-git branch -M main
-git add .
-git commit -am "Update"
-git push -u origin main
--- SYNC_REMOTE.sh
+++
@@ -1,3 +0,0 @@
-# pull remote changes, merge them and reset master branch to the remote changes.
-git fetch --all
-git reset --hard origin/main
